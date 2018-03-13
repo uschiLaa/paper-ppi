4b18d2c0f06f66b4f0ef2ab9447bc29783050efd
Ursula Laa <uschi.laa@gmx.at>
2018-03-13 11:01:18 +1100

    Bigger dataset to study temporal trace, differences in projections and binning

--- testMultipleFits.R
+++ testMultipleFits.R
@@ -3,6 +3,10 @@ numbat_circle <- read_csv("Draft/data/numbat.csv")
 numbat_only <- numbat_circle %>% filter(group=="A") %>% select(-group)
 circle_only <- numbat_circle %>% filter(group=="B") %>% select(-group)
 
+physEx <- read_csv("~/smodels/smodels-master/testStop/summaryRvalExpected.csv")
+ex1 <- physEx %>% filter(group=="BRt_75_BRb_25") %>% select(-group,-mst,-mchi,-cat)
+
+
 scagIndex2 <- function(proj,dataF,scagType) {
   proj <- matrix(proj, ncol=2, byrow=FALSE)
   proj <- tourr:::orthonormalise(proj)
@@ -13,7 +17,8 @@ scagIndex2 <- function(proj,dataF,scagType) {
   return(sR[scagType])
 }
 
-ix <- length(circle_only)
+#ix <- length(circle_only)
+ix <- length(ex1)
 ntries <- 10
 
 lower <- rep(-1,2*ix)
@@ -26,8 +31,9 @@ i <- 1
 while(i<=ntries){
   par0 <- runif(2*ix, min = 0, max = 1)
   opt <- hjkb(par0, scagIndex2, lower=lower, upper=upper,
-              control = list(maximize=TRUE, tol=0.001, 
-                             info=TRUE),dataF=circle_only, scagType="Skinny")
+              control = list(maximize=TRUE, tol=0.001,
+                             info=TRUE),dataF=ex1, scagType="Skinny")
+  #                           info=TRUE),dataF=circle_only, scagType="Skinny")
   idxV[i] <- opt$value
   prjL[[i]] <- tourr:::orthonormalise(matrix(opt$par, ncol=2, byrow=FALSE))
   i <- i+1
@@ -39,5 +45,6 @@ print(idxV[i1])
 
 plannedT <- planned_tour(prjL[i1])
 quartz()
-animate(numbat_only, plannedT)
+animate(ex1,plannedT)
+#animate(circle_only, plannedT)
 
